12060e40a67fba8d4b8110c64643695a439fa8fe
Nore_HT <Hamoutahranore@gmail.com>
2021-08-30 18:14:36 +0000

    fullcalendar2

--- Desktop/src/pages/App_bar.js
+++ Desktop/src/pages/App_bar.js
@@ -15,7 +15,12 @@ import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import DateRangeIcon from '@material-ui/icons/DateRange';
 import MailIcon from '@material-ui/icons/Mail';
 import Agenda from './Agenda';
-
+import DuoIcon from '@material-ui/icons/Duo';
+import HdrStrongIcon from '@material-ui/icons/HdrStrong';
+import green from "@material-ui/core/colors/green";
+import red from "@material-ui/core/colors/red";
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -81,12 +86,56 @@ export default function App_bar() {
          
         </div>
       </Drawer>
-      <main className={classes.content}>
-        <Toolbar />
-        <Typography paragraph>
-         <Agenda/>
-        </Typography>
+      <main className="container-fluid">
+        <div className="row">
+          <div className="col-md-3">
+            <div className="iq-card">
+              <div className="iq-card-header d-flex justify-content-between">
+                <div className="iq_header-title">
+                  <h4 className="card-title">Consultation</h4>
+                </div>
+                <div className="iq-card-header-toolbar d-flex align-items-center">
+                 <a>
+                   <BorderColorIcon/>
+                 </a>
+                </div>
+              </div>
+              <div className="iq-card-body">
+                <ul className="m-0 p-0 job-classification">
+                  <li><HdrStrongIcon style={{ color: "green" }}/>1ere consultation</li>
+                  <li><HdrStrongIcon style={{ color: "red" }}/>Controle</li>
+                  <li><HdrStrongIcon/>Deja consulté</li>
+                </ul>
+              </div>
+            </div>
+            <div className="iq-card">
+              <div className="iq-card-header d-flex justify-content-between">
+                <div className="iq_header-title">
+                  <h4 className="card-title">Prochaine Consultation</h4>
+                </div>
+                <div className="iq-card-header-toolbar d-flex align-items-center">
+                 <a>
+                 <DuoIcon/>
+                 </a>
+                </div>
+              </div>
+              <div className="iq-card-body">
+                <ul className="m-0 p-0 job-classification">
+                  <li> Bla bla</li>
+                  <li> blabla</li>
+                  <li> blaablaa</li>
+                </ul>
+              </div>
+            </div>
+          
+          </div>
+          <div className="col-md-9">
+           
         
+         <Agenda/>
+        
+        </div>
+        </div>
       </main>
     </div>
   );
